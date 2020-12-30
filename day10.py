cc58f670589bf75052293e6397c6f4ba94cc6d32
Tony <tongqingwu@gmail.com>
2020-12-30 16:07:03 -0600

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,6 +1,7 @@
 cnt_one = 0
 cnt_three = 0
 
+
 def get_nums_from_file(f_name):
     nums = []
     with open(f_name) as f:
@@ -8,18 +9,68 @@ def get_nums_from_file(f_name):
             line = line.rstrip()
             nums.append(int(line))
     nums.sort()
-    print(nums)
+    # print(nums)
     return nums
 
 
 def part2(nums):
+    # print(find_p_no_recursive(4))
+    p = 1
+    l_slices = get_slices(nums)
+    # print(l_slices)
+    for s in l_slices:
+        p *= find_p_no_recursive(len(s))
 
-    print('Part2: {}'.format(list_nums))
-    print('Part2: {}'.format(len(set(list_nums))))
+    print('Part2: {}'.format(p))
 
 
-# slow 
-def def find_p(n):
+def get_one_and_three_jolts(nums):
+    cnt_diffs(0, nums[0])
+    i = 0 
+    while i < (len(nums) - 1): 
+        cnt_diffs(nums[i], nums[i+1])
+        i += 1
+
+
+def cnt_diffs(j_one, j_two):
+    global cnt_one
+    global cnt_three
+    diff_j = j_two - j_one 
+
+    if diff_j == 1:
+        cnt_one += 1
+    elif diff_j == 3:
+        cnt_three += 1
+
+
+def part1(nums):
+    get_one_and_three_jolts(nums)
+    # print('cnt one {}'.format(cnt_one))
+    # last 3 add one more:
+    # print('cnt three {}'.format(cnt_three + 1))
+    print('Part 1: {}'.format(cnt_one * (cnt_three + 1)))
+
+
+def get_slices(nums):
+    # print(len(nums))
+    l_slices = []
+    tmp_s = []
+    for i in range(len(nums)):
+        if i == 0:
+            tmp_s.append(nums[0])
+        else:
+            if nums[i] - nums[i - 1] == 3:
+                l_slices.append(tmp_s)
+                tmp_s = []
+
+            tmp_s.append(nums[i])
+
+    l_slices.append(tmp_s)
+    return l_slices
+
+
+# slow
+def find_p(n):
     if n == 1:
         return 1
     elif n == 2:
@@ -32,54 +83,24 @@ def def find_p(n):
 
 # quick
 def find_p_no_recursive(n):
-    list1 = [1,1,2,4]
-    while (n != len(list1)):
-        list1.append(list1[-1]+list1[-2]+list1[-3])
-    print(list1)
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n == 3:
+        return 2
+    elif n == 4:
+        return 4
 
-    return list1[-1]
+    p_list = [1,1,2,4]
+    while n != len(p_list):
+        p_list.append(p_list[-1]+p_list[-2]+p_list[-3])
+    # print(p_list)
 
-
-def get_one_and_three_jolts(nums):
-    cnt_diffs(0, nums[0])
-    i = 0 
-    while i < (len(nums) - 1): 
-        #print('i is {}'.format(i))
-        #print('num now {}'.format(nums[i]))
-        #print('num next now {}'.format(nums[i + 1]))
-        cnt_diffs(nums[i], nums[i+1])
-        i += 1
-
-
-def cnt_diffs(j_one, j_two):
-    global cnt_one
-    global cnt_three
-    # print('j2 {}'.format(j_two))
-    # print('j1 {}'.format(j_one))
-    diff_j = j_two - j_one 
-
-    if diff_j == 1:
-       cnt_one += 1
-    elif diff_j == 3:
-       cnt_three += 1 
-
-
-def part1(nums):
-    get_one_and_three_jolts(nums)
-    print('cnt one {}'.format(cnt_one))
-    # last 3 add one more:
-    print('cnt three {}'.format(cnt_three + 1))
-    print('Part 1: {}'.format(cnt_one * (cnt_three + 1)))
+    return p_list[-1]
 
 
 def main():
-    # nums = get_nums_from_file('d10.txt')
-    nums = get_nums_from_file('da10.txt')
-    # nums = get_nums_from_file('day10.txt')
-    nums.insert(0, 0)
-    part1(nums)
-    part2(nums)
-
-
-if __name__ == '__main__':
-    main()
+    # nums = get_nums_from_file('da10.txt')
+    # nums = get_nums_from_file('da10.txt')
+    nums = get_nums_fro
